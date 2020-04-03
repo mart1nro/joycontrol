f2a0e866775cc20c11f32c4c184dc69027d1c150
Cambridge Yang <jq_yang@mit.edu>
2020-04-03 12:16:03 -0400

    cleanup

--- run_controller_cli.py
+++ run_controller_cli.py
@@ -10,22 +10,18 @@ from joycontrol.command_line_interface import ControllerCLI
 from joycontrol.controller import Controller
 from joycontrol.memory import FlashMemory
 from joycontrol.protocol import controller_protocol_factory
-from joycontrol.server import create_hid_server, create_reconnection
+from joycontrol.server import create_hid_server
 from joycontrol.report import InputReport
 
 logger = logging.getLogger(__name__)
 
 
-async def _main(controller, console_bt_addr=None, capture_file=None, spi_flash=None, device_id=None):
+async def _main(controller, reconnect_bt_addr=None, capture_file=None, spi_flash=None, device_id=None):
     factory = controller_protocol_factory(controller, spi_flash=spi_flash)
     ctl_psm, itr_psm = 17, 19
-    if console_bt_addr is None:
-        transport, protocol = await create_hid_server(factory, 
-            ctl_psm=ctl_psm, itr_psm=itr_psm, capture_file=capture_file, device_id=device_id)
-    else:
-        transport, protocol = await create_reconnection(factory, 
-            console_bt_addr,
-            ctl_psm=ctl_psm, itr_psm=itr_psm, capture_file=capture_file)
+    transport, protocol = await create_hid_server(factory, 
+        reconnect_bt_addr=reconnect_bt_addr,
+        ctl_psm=ctl_psm, itr_psm=itr_psm, capture_file=capture_file, device_id=device_id)
 
     controller_state = protocol.get_controller_state()
 
@@ -50,7 +46,8 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--log')
     parser.add_argument('-d', '--device_id')
     parser.add_argument('--spi_flash')
-    parser.add_argument('--console-bt-addr', type=str, default=None)
+    parser.add_argument('-r', '--reconnect_bt_addr', type=str, default=None, 
+        help='The Switch console bluetooth address, for reconnecting as an already paired controller')
     args = parser.parse_args()
 
     if args.controller == 'JOYCON_R':
@@ -84,7 +81,7 @@ if __name__ == '__main__':
         loop = asyncio.get_event_loop()
         loop.run_until_complete(
             _main(controller, 
-                console_bt_addr=args.console_bt_addr, 
+                reconnect_bt_addr=args.reconnect_bt_addr, 
                 capture_file=capture_file, 
                 spi_flash=spi_flash, 
                 device_id=args.device_id
