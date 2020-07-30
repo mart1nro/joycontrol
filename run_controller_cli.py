2c99ee36cb4e9c6508ff432b565e2477b5232ca5
hiprof <45518409+hiprof@users.noreply.github.com>
2020-07-30 16:48:00 +0200

    Update run_controller_cli.py

--- run_controller_cli.py
+++ run_controller_cli.py
@@ -175,12 +175,15 @@ async def _main(args):
         if args.color:
             if Spicolor.var_custom_SPI(args.spi_flash, args.color):
                 spi_flash = FlashMemory(Spicolor.var_custom_SPI(args.spi_flash, args.color))
+                logger.info("Color Changed")
             else:
                 with open(args.spi_flash, 'rb') as spi_flash_file:
                     spi_flash = FlashMemory(spi_flash_file.read())
+                    logger.info("EX1")
         else:
             with open(args.spi_flash, 'rb') as spi_flash_file:
                 spi_flash = FlashMemory(spi_flash_file.read())
+                logger.info("EX2")
     else:
         # Create memory containing default controller stick calibration
         spi_flash = FlashMemory()
